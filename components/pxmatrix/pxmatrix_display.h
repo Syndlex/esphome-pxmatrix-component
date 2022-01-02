55604dff7403c214b0b7582ba3d3c8d2bb2876fb
Marcel Feix <marcel@feixe.de>
2022-01-02 21:53:45 +0100

    set gfx lib

--- components/pxmatrix/pxmatrix_display.h
+++ components/pxmatrix/pxmatrix_display.h
@@ -3,6 +3,7 @@
 #include "esphome/core/component.h"
 #include "esphome/components/display/display_buffer.h"
 #include "esphome/core/log.h"
+#include "esphome/core/gpio.h"
 
 #define PxMATRIX_double_buffer true
 
