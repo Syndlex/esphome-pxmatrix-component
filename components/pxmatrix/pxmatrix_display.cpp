89801ece72eeed6cbc3be14b8a1b7b5714359144
Marcel Feix <marcel@feixe.de>
2022-01-06 21:43:40 +0100

    sdf

--- components/pxmatrix/pxmatrix_display.cpp
+++ components/pxmatrix/pxmatrix_display.cpp
@@ -25,7 +25,7 @@ namespace pxmatrix_display {
   #endif
 
 
-void display_updater() { pxMatrix->display(60); }
+void display_updater() { pxMatrix->display(40); }
 
 float PxmatrixDisplay::get_setup_priority() const { return setup_priority::PROCESSOR; }
 
