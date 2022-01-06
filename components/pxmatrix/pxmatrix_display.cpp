6149d9e9ffae6efe0f4fb02519cde16024291ea1
Marcel Feix <marcel@feixe.de>
2022-01-06 22:29:57 +0100

    asdf

--- components/pxmatrix/pxmatrix_display.cpp
+++ components/pxmatrix/pxmatrix_display.cpp
@@ -25,7 +25,7 @@ namespace pxmatrix_display {
   #endif
 
 
-void display_updater() { pxMatrix->display(40); }
+void display_updater() { pxMatrix->display(); }
 
 float PxmatrixDisplay::get_setup_priority() const { return setup_priority::PROCESSOR; }
 
@@ -49,7 +49,7 @@ void PxmatrixDisplay::setup() {
   ESP_LOGI(TAG, "Finished Setup");
 
   #ifdef ESP8266
-    display_ticker.attach(0.008, display_updater);
+    display_ticker.attach(0.007, display_updater);
   #endif
 
   #ifdef ESP32
