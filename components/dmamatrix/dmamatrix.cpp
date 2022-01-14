06efdc22f1f7440bd25a2cb4dbe2586477888193
Marcel Feix <marcel@feixe.de>
2022-01-14 22:49:38 +0100

    sdf

--- components/dmamatrix/dmamatrix.cpp
+++ components/dmamatrix/dmamatrix.cpp
@@ -58,39 +58,39 @@ void DmaMatrixDisplay::update() {
 
 void HOT DmaMatrixDisplay::display() {}
 
-void dmamatrixDisplay::set_pin_latch(InternalGPIOPin *pin_latch) { this->pin_latch_ = pin_latch; }
+void DmaMatrixDisplay::set_pin_latch(InternalGPIOPin *pin_latch) { this->pin_latch_ = pin_latch; }
 
-void dmamatrixDisplay::set_pin_r1(InternalGPIOPin *pin_r1) { this->pin_r1_ = pin_r1; }
+void DmaMatrixDisplay::set_pin_r1(InternalGPIOPin *pin_r1) { this->pin_r1_ = pin_r1; }
 
-void dmamatrixDisplay::set_pin_r2(InternalGPIOPin *pin_r2) { this->pin_r2_ = pin_r2; }
+void DmaMatrixDisplay::set_pin_r2(InternalGPIOPin *pin_r2) { this->pin_r2_ = pin_r2; }
 
-void dmamatrixDisplay::set_pin_g1(InternalGPIOPin *pin_g1) { this->pin_g1_ = pin_g1; }
+void DmaMatrixDisplay::set_pin_g1(InternalGPIOPin *pin_g1) { this->pin_g1_ = pin_g1; }
 
-void dmamatrixDisplay::set_pin_g2(InternalGPIOPin *pin_g2) { this->pin_g2_ = pin_g2; }
+void DmaMatrixDisplay::set_pin_g2(InternalGPIOPin *pin_g2) { this->pin_g2_ = pin_g2; }
 
-void dmamatrixDisplay::set_pin_b1(InternalGPIOPin *pin_b1) { this->pin_b1_ = pin_b1; }
+void DmaMatrixDisplay::set_pin_b1(InternalGPIOPin *pin_b1) { this->pin_b1_ = pin_b1; }
 
-void dmamatrixDisplay::set_pin_b2(InternalGPIOPin *pin_b2) { this->pin_b2_ = pin_b2; }
+void DmaMatrixDisplay::set_pin_b2(InternalGPIOPin *pin_b2) { this->pin_b2_ = pin_b2; }
 
-void dmamatrixDisplay::set_pin_clk(InternalGPIOPin *pin_clk) { this->pin_clk_ = pin_clk; }
+void DmaMatrixDisplay::set_pin_clk(InternalGPIOPin *pin_clk) { this->pin_clk_ = pin_clk; }
 
-void dmamatrixDisplay::set_pin_a(InternalGPIOPin *pin_a) { this->pin_a_ = pin_a; }
+void DmaMatrixDisplay::set_pin_a(InternalGPIOPin *pin_a) { this->pin_a_ = pin_a; }
 
-void dmamatrixDisplay::set_pin_b(InternalGPIOPin *pin_b) { this->pin_b_ = pin_b; }
+void DmaMatrixDisplay::set_pin_b(InternalGPIOPin *pin_b) { this->pin_b_ = pin_b; }
 
-void dmamatrixDisplay::set_pin_c(InternalGPIOPin *pin_c) { this->pin_c_ = pin_c; }
+void DmaMatrixDisplay::set_pin_c(InternalGPIOPin *pin_c) { this->pin_c_ = pin_c; }
 
-void dmamatrixDisplay::set_pin_d(InternalGPIOPin *pin_d) { this->pin_d_ = pin_d; }
+void DmaMatrixDisplay::set_pin_d(InternalGPIOPin *pin_d) { this->pin_d_ = pin_d; }
 
-void dmamatrixDisplay::set_pin_e(InternalGPIOPin *pin_e) { this->pin_e_ = pin_e; }
+void DmaMatrixDisplay::set_pin_e(InternalGPIOPin *pin_e) { this->pin_e_ = pin_e; }
 
-void dmamatrixDisplay::set_pin_oe(InternalGPIOPin *pin_oe) { this->pin_oe_ = pin_oe; }
+void DmaMatrixDisplay::set_pin_oe(InternalGPIOPin *pin_oe) { this->pin_oe_ = pin_oe; }
 
-void dmamatrixDisplay::set_width(uint8_t width) { this->width_ = width; }
+void DmaMatrixDisplay::set_width(uint8_t width) { this->width_ = width; }
 
-void dmamatrixDisplay::set_height(uint8_t height) { this->height_ = height; }
+void DmaMatrixDisplay::set_height(uint8_t height) { this->height_ = height; }
 
-void dmamatrixDisplay::set_brightness(uint8_t brightness) { this->brightness_ = brightness; }
+void DmaMatrixDisplay::set_brightness(uint8_t brightness) { this->brightness_ = brightness; }
 
 int DmaMatrixDisplay::get_width_internal() { return this->width_; }
 
